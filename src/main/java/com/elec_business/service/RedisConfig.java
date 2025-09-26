4fc7644aca39a08e72c07e538e245e8d22f936ce
Nahima697 <nahima.toumi697@gmail.com>
2025-09-26 14:59:15 +0200

    redis profile dev

--- src/main/java/com/elec_business/service/RedisConfig.java
+++ src/main/java/com/elec_business/service/RedisConfig.java
@@ -2,12 +2,14 @@ package com.elec_business.service;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 
 @Configuration
+@Profile("dev")
 public class RedisConfig {
 
     @Bean
