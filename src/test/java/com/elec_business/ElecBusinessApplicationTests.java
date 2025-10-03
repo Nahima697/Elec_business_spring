88b14c32f54143786269d2a06ffdcc331575dc6c
Nahima697 <nahima.toumi697@gmail.com>
2025-10-03 11:19:36 +0200

    modify pom

--- src/test/java/com/elec_business/ElecBusinessApplicationTests.java
+++ src/test/java/com/elec_business/ElecBusinessApplicationTests.java
@@ -1,13 +1,14 @@
-// package com.elec_business.elec_business;
+ package com.elec_business;
 
-// import org.junit.jupiter.api.Test;
-// import org.springframework.boot.test.context.SpringBootTest;
+ import org.junit.jupiter.api.Test;
+ import org.springframework.boot.test.context.SpringBootTest;
 
-// @SpringBootTest
-// class ElecBusinessApplicationTests {
+ @SpringBootTest(properties = "spring.profiles.active=test")
+ class ElecBusinessApplicationTests {
 
-// 	@Test
-// 	void contextLoads() {
-// 	}
+ 	@Test
+ 	void contextLoads() {
+        // TODO document why this method is empty
+    }
 
-// }
+ }
