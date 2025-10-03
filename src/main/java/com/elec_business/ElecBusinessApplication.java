564ae5047f126c21671b5a4dd991d5004c764e0c
Nahima697 <nahima.toumi697@gmail.com>
2025-10-03 15:26:34 +0200

    add log url database

--- src/main/java/com/elec_business/ElecBusinessApplication.java
+++ src/main/java/com/elec_business/ElecBusinessApplication.java
@@ -1,5 +1,7 @@
 package com.elec_business;
 
+import jakarta.annotation.PostConstruct;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
@@ -9,6 +11,13 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 @EnableJpaRepositories(basePackages = "com.elec_business")
 @EnableScheduling
 public class ElecBusinessApplication {
+	@Value("${spring.datasource.url:NOT_FOUND}")
+	private String dbUrl;
+
+	@PostConstruct
+	public void logDbUrl() {
+		System.out.println("🔍 spring.datasource.url = " + dbUrl);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(ElecBusinessApplication.class, args);
