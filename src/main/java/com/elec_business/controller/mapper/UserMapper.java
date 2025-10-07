a428b5aeff4131ed7fde79f1e6ddb24105bb2bbe
Nahima697 <nahima.toumi697@gmail.com>
2025-10-07 23:36:15 +0200

    modify userMapper

--- src/main/java/com/elec_business/controller/mapper/UserMapper.java
+++ src/main/java/com/elec_business/controller/mapper/UserMapper.java
@@ -6,7 +6,7 @@ import org.mapstruct.*;
 
 @Mapper(unmappedTargetPolicy = ReportingPolicy.IGNORE, componentModel = MappingConstants.ComponentModel.SPRING)
 public interface UserMapper {
-    @Mapping(source = "roleId", target = "role")
+    @Mapping(source = "roleId", target = "role.id")
     User toEntity(UserRegisterDto userRegisterDto);
     UserRegisterDto toDto(User appUser);
     UserDTO toDTO(User appUser);
