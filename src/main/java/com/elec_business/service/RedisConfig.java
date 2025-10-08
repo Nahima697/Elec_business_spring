ed84e0eee3457d6680ca3db3e034495640ff17b6
Nahima697 <nahima.toumi697@gmail.com>
2025-10-08 12:29:01 +0200

    redis_url

--- src/main/java/com/elec_business/service/RedisConfig.java
+++ src/main/java/com/elec_business/service/RedisConfig.java
@@ -3,35 +3,44 @@ package com.elec_business.service;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.data.redis.connection.ReactiveRedisConnectionFactory;
-import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
-import org.springframework.data.redis.core.ReactiveRedisTemplate;
 import org.springframework.data.redis.core.RedisTemplate;
-import org.springframework.data.redis.core.StringRedisTemplate;
-import org.springframework.data.redis.serializer.RedisSerializationContext;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
 
+import java.net.URI;
+import java.net.URISyntaxException;
 
 @Configuration
-
 public class RedisConfig {
-    @Value("${spring.data.redis.host:host}")
-    private String host;
 
-    @Value("${spring.data.redis.port:6379}")
-    private Integer port;
+    @Value("${REDIS_URL:}")
+    private String redisUrl;
 
     @Bean
-    public RedisConnectionFactory redisConnectionFactory() {
-        RedisStandaloneConfiguration serverConfig = new RedisStandaloneConfiguration(host, port);
-        return new LettuceConnectionFactory(serverConfig);
+    public LettuceConnectionFactory redisConnectionFactory() throws URISyntaxException {
+        if (redisUrl == null || redisUrl.isEmpty()) {
+            throw new IllegalStateException("REDIS_URL environment variable is not set!");
+        }
+
+        URI redisUri = new URI(redisUrl);
+
+        String host = redisUri.getHost();
+        int port = redisUri.getPort();
+
+        RedisStandaloneConfiguration config = new RedisStandaloneConfiguration();
+        config.setHostName(host);
+        config.setPort(port);
+
+        return new LettuceConnectionFactory(config);
     }
 
     @Bean
-    ReactiveRedisTemplate<String, String> ReactiveRedisTemplate(ReactiveRedisConnectionFactory connectionFactory) {
-        return new ReactiveRedisTemplate<>(connectionFactory, RedisSerializationContext.string());
+    public RedisTemplate<String, String> redisTemplate(LettuceConnectionFactory connectionFactory) {
+        RedisTemplate<String, String> template = new RedisTemplate<>();
+        template.setConnectionFactory(connectionFactory);
+        template.setKeySerializer(new StringRedisSerializer());
+        template.setValueSerializer(new StringRedisSerializer());
+        return template;
     }
 }
-
