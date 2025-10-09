468582b0596df4fbe7f73d75a9c2abbfc3038905
Nahima697 <nahima.toumi697@gmail.com>
2025-10-09 09:51:48 +0200

    sendgrid email

--- src/main/java/com/elec_business/service/EmailVerificationService.java
+++ src/main/java/com/elec_business/service/EmailVerificationService.java
@@ -2,9 +2,10 @@ package com.elec_business.service;
 
 import com.elec_business.entity.User;
 
+import java.io.IOException;
 import java.util.UUID;
 
 public interface EmailVerificationService {
-    void sendVerificationToken(String userId, String email);
+    void sendVerificationToken(String userId, String email) throws IOException;
     User verifyEmail(String userId, String token);
 }
