e40155bf2629bdc0c2270d89e688f0f40d750563
Nahima697 <nahima.toumi697@gmail.com>
2025-10-10 18:04:55 +0200

    use mockito bean instead of autowired to mock mockmvc

--- src/test/java/com/elec_business/controller/BookingControllerTest.java
+++ src/test/java/com/elec_business/controller/BookingControllerTest.java
@@ -13,20 +13,17 @@ import org.junit.jupiter.api.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.Import;
 import org.springframework.http.MediaType;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.ActiveProfiles;
-import org.springframework.test.context.DynamicPropertyRegistry;
-import org.springframework.test.context.DynamicPropertySource;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.List;
 import com.elec_business.config.TestcontainersConfiguration;
-import org.testcontainers.containers.PostgreSQLContainer;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
@@ -43,7 +40,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
     @Autowired
     public TestDataLoader testDataLoader;
 
-    @Autowired
+    @MockitoBean
     MockMvc mvc;
 
     @Autowired
