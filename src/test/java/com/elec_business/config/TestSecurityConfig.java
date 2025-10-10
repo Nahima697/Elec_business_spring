dfd5a2cdd9b7195ae09168c4d6a467e6e483bd7f
Nahima697 <nahima.toumi697@gmail.com>
2025-10-10 20:27:11 +0200

    add entitymanger in testsecurity config

--- src/test/java/com/elec_business/config/TestSecurityConfig.java
+++ src/test/java/com/elec_business/config/TestSecurityConfig.java
@@ -1,16 +1,24 @@
 package com.elec_business.config;
 
+import com.elec_business.security.CustomUserDetailsService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @Profile("test")
 @EnableWebSecurity
+@RequiredArgsConstructor
 public class TestSecurityConfig {
+    private final CustomUserDetailsService userDetailsService;
+
 
     @Bean
     public SecurityFilterChain testSecurityFilterChain(HttpSecurity http) throws Exception {
@@ -19,4 +27,11 @@ public class TestSecurityConfig {
 
         return http.build();
     }
+    @Bean
+    public AuthenticationManager authenticationManager(HttpSecurity http, PasswordEncoder passwordEncoder) throws Exception {
+        AuthenticationManagerBuilder authenticationManagerBuilder = http.getSharedObject(AuthenticationManagerBuilder.class);
+        authenticationManagerBuilder.userDetailsService(userDetailsService).passwordEncoder(passwordEncoder);
+        return authenticationManagerBuilder.build();
+    }
+
 }
