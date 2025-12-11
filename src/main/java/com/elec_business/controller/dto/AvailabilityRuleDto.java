65863116676ef79b7f498044609afdd3538dccd2
Nahima <nahima.toumi697@gmail.com>
2025-12-11 13:13:32 +0100

    modif AvailabilityRuleDto

--- src/main/java/com/elec_business/controller/dto/AvailabilityRuleDto.java
+++ src/main/java/com/elec_business/controller/dto/AvailabilityRuleDto.java
@@ -1,29 +1,23 @@
 package com.elec_business.controller.dto;
 
-import com.elec_business.entity.ChargingStation;
+
 import jakarta.validation.constraints.NotNull;
 import lombok.Data;
-import lombok.Getter;
-import lombok.Setter;
-
-import java.time.Instant;
 import java.time.LocalTime;
 
 @Data
-@Getter
-@Setter
 public class AvailabilityRuleDto {
 
-    @NotNull(message ="La station ne doit pas être vide")
-    private ChargingStation chargingStation;
+    @NotNull(message = "L'ID de la station est obligatoire")
+    private String stationId;
 
     @NotNull
     private Integer dayOfWeek;
 
     @NotNull
     private LocalTime startTime;
+
     @NotNull
     private LocalTime endTime;
-    @NotNull
-    private Instant createdAt;
+
 }
