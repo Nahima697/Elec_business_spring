fa622a35f3d690fca79d3169d2fa7a938fd345cf
Nahima-Toumi- <104007362+Nahima697@users.noreply.github.com>
2025-12-17 13:46:20 +0100

    Import TestcontainersConfiguration in tests
    Added import for TestcontainersConfiguration to the test class.

--- src/test/java/com/elec_business/ElecBusinessApplicationTests.java
+++ src/test/java/com/elec_business/ElecBusinessApplicationTests.java
@@ -1,14 +1,15 @@
- package com.elec_business;
+package com.elec_business;
 
- import org.junit.jupiter.api.Test;
- import org.springframework.boot.test.context.SpringBootTest;
+import com.elec_business.config.TestcontainersConfiguration; // Ajoute l'import
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import; // Ajoute l'import
 
- @SpringBootTest(properties = "spring.profiles.active=test")
- class ElecBusinessApplicationTests {
+@SpringBootTest(properties = "spring.profiles.active=test")
+@Import(TestcontainersConfiguration.class) 
+class ElecBusinessApplicationTests {
 
- 	@Test
- 	void contextLoads() {
-        // TODO document why this method is empty
+    @Test
+    void contextLoads() {
     }
-
- }
+}
