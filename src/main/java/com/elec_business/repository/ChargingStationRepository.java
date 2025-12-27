2f14e2527d567ce410e13363aadb155b9795124c
Nahima <nahima.toumi697@gmail.com>
2025-12-27 16:05:54 +0100

    change getbylocation id in chargingstation repository

--- src/main/java/com/elec_business/repository/ChargingStationRepository.java
+++ src/main/java/com/elec_business/repository/ChargingStationRepository.java
@@ -2,8 +2,10 @@ package com.elec_business.repository;
 
 import com.elec_business.entity.AvailabilityRule;
 import com.elec_business.entity.ChargingStation;
+import io.lettuce.core.dynamic.annotation.Param;
 import org.springframework.data.domain.Page;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import org.springframework.data.domain.Pageable;
@@ -13,7 +15,12 @@ import java.util.List;
 @Repository
 public interface ChargingStationRepository  extends JpaRepository<ChargingStation, String> {
     ChargingStation findChargingStationByName(String name);
-    List<ChargingStation>  findByLocation_Id(String locationId);
+    @Query("SELECT DISTINCT cs FROM ChargingStation cs " +
+            "LEFT JOIN FETCH cs.reviews " +
+            "LEFT JOIN FETCH cs.location loc " +
+            "LEFT JOIN FETCH loc.user " +
+            "WHERE loc.id = :id")
+    List<ChargingStation> findByLocation_Id(@Param("id") String id);
     ChargingStation findByAvailabilityRulesContaining(AvailabilityRule availabilityRule);
     Page<ChargingStation> findAll(Pageable pageable);
 }
