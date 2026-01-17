2d54655da4c284e86cfbc78bcf2d6cb2e7e1701b
Nahima <nahima.toumi697@gmail.com>
2026-01-17 21:26:52 +0100

    modif AvailaibilyRuleMapper

--- src/main/java/com/elec_business/controller/mapper/AvailabilityRuleMapper.java
+++ src/main/java/com/elec_business/controller/mapper/AvailabilityRuleMapper.java
@@ -7,10 +7,15 @@ import org.mapstruct.Mapping;
 
 @Mapper(componentModel = "spring")
 public interface AvailabilityRuleMapper {
+
     @Mapping(source = "stationId", target = "chargingStation.id")
-    public   abstract AvailabilityRule toEntity(AvailabilityRuleDto dto) ;
+    @Mapping(source = "dayOfWeek", target = "dayOfWeek")
+    @Mapping(source = "startTime", target = "startTime")
+    @Mapping(source = "endTime", target = "endTime")
+    AvailabilityRule toEntity(AvailabilityRuleDto dto);
+
     @Mapping(source = "chargingStation.id", target = "stationId")
     @Mapping(source = "chargingStation.name", target = "stationName")
-    public   abstract AvailabilityRuleDto toDto(AvailabilityRule entity) ;
-
+    AvailabilityRuleDto toDto(AvailabilityRule entity);
 }
+
