cb0cd5ad6f534e34c052f8c1491b06633f2b4b97
Nahima <nahima.toumi697@gmail.com>
2026-02-15 02:01:46 +0100

    add enabledMethodeSecurity

--- src/main/java/com/elec_business/security/SecurityConfig.java
+++ src/main/java/com/elec_business/security/SecurityConfig.java
@@ -9,6 +9,7 @@ import org.springframework.context.annotation.Profile;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -26,6 +27,7 @@ import java.util.List;
 @Configuration
 @Profile("!test")
 @EnableWebSecurity
+@EnableMethodSecurity
 @RequiredArgsConstructor
 public class SecurityConfig {
 
